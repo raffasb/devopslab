85252c3bfc7d646287194187d295103a2823a525
Rafael Barros <raffasb@users.noreply.github.com>
2023-11-23 08:35:21 -0300

    feat: entrega final

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello World, this is Rafael Barros from 11ASOO - Ultima aula ao vivo")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World, this is Rafael Barros from 11ASOO - Entrega Final do Solution Sprint 5")
